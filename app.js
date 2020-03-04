41a651378f9dc75d41ba79f3e5bd65f5325e03c9
James Tracy <jtracy.emmet@gmail.com>
2020-03-04 02:08:20 -0500

    pixels

--- app.js
+++ app.js
@@ -6,55 +6,29 @@ const cors = require('cors');
 const pixelsService = require('./service.js');
 const app = express();
 const port = process.env.PORT || 8080;
-
 app.use(cors());
 app.use(bodyParser.json({limit: '5mb'}));
 
-let basePixels = [];
-let memPixels = [];
-let count = 0;
-const refresh = 5000;
-
-app.get('/pixels', async (req, res) => {
-  res.send(basePixels.concat(memPixels));
-});
+let memPixels;
 
 app.post('/pixels', (req, res) => {
-  res.send(memPixels);
-  count += req.body.pixels.length;
-  addPixelsToMem(req.body.pixels);
-  if(req.body.pixels.length > 0) pixelsService.savePixels(req.body.pixels);
-  if(count > refresh) {
-    count = 0;
-    sendMemToBase(); // This may cause some clients to lose data, to compensate clients will load every minute or so.
+  const end = memPixels.length;
+  const response = {
+    index: end,
+    pixels: memPixels.slice(req.body.index, end),
   }
+  res.send();
+
 });
 
 app.get('/ping', (req, res) => {
   res.send('ok');
 });
 
-app.listen(port);
-
-async function loadPixelsToMem() {
+async function loadPixels() {
   const pixels = await pixelsService.getAllPixels();
-  basePixels = pixels;
-  memPixels = [];
+  memPixels = pixels;
 }
 
-function sendMemToBase() {
-  let i;
-  for(i = 0; i < memPixels.length; i += 1) {
-    basePixels.push(memPixels[i]);
-  }
-  memPixels = [];
-}
-
-function addPixelsToMem(pixels) {
-  let i;
-  for(i = 0; i < pixels.length; i += 1) {
-    memPixels.push(pixels[i]);
-  }
-}
-
-loadPixelsToMem();
+loadPixels();
+app.listen(port);
